de300b342f657f36c38cbb02536dd51cf10e0022
Steven Eck <steeck1@yahoo.com>
2020-02-21 21:03:41 -0500

    Added my first class, Test.java

---
+++ TestProject/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello You!");
+
+	}
+
+}
